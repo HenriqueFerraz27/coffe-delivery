2044f8280b9ea73612caed5972bb24676d371bd6
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2024-01-16 16:20:20 -0800

    💄style(styled-components): dark theme

---
+++ src/styles/themes.ts
@@ -0,0 +1,25 @@
+import { brandColor as brand, typography } from './index'
+
+export const darkTheme = {
+  color: {
+    brand,
+    basic: {
+      base: {
+        primary: '#E1E1E6',
+        secondary: '#C4C4CC',
+        tertiary: '#8D8D99',
+      },
+      support: {
+        primary: '#7C7C8A',
+        secondary: '#505059',
+        tertiary: '#323238',
+      },
+      auxiliary: {
+        primary: '#29292E',
+        secondary: '#202024',
+        tertiary: '#121214',
+      },
+    },
+  },
+  typography,
+}
---
+++ src/styles/tokens.ts
@@ -0,0 +1,38 @@
+export const brandColor = {
+  yellow: {
+    primary: '#F1E9C9',
+    secondary: '#DBAC2C',
+    tertiary: '#C47F17',
+  },
+  purple: {
+    primary: '#EBE5F9',
+    secondary: '#8047F8',
+    tertiary: '#4B2995',
+  },
+}
+
+export const typography = {
+  family: {
+    baloo2: '"Baloo 2", sans-serif',
+    roboto: 'Roboto, sans-serif',
+  },
+  size: {
+    '2xs': '0.625rem',
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '2rem',
+    '4xl': '3rem',
+  },
+  weight: {
+    regular: 400,
+    bold: 700,
+    extraBold: 800,
+  },
+  lineHeight: {
+    base: 1.6,
+  },
+}
