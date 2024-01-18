71817160f6b4c23f6ee46d557aa5006043bc6687
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2024-01-17 20:32:12 -0800

    🏗feat: pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
+import { ThemeProvider } from 'styled-components'
+import { darkTheme } from './styles'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
+function App() {
   return (
-    <>
-      Hello World - Getting Started
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
