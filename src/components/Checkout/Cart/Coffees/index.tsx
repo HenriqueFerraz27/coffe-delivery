d048946c039f3de589b3f05d529b0b27ee76bdce
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2024-01-23 23:13:35 -0800

    🏗feat(checkout-cart): coffee item - component

--- src/components/Checkout/Cart/Coffees/index.tsx
+++ src/components/Checkout/Cart/Coffees/index.tsx
@@ -1,100 +1,11 @@
 import * as S from './styles'
-import * as Styled from '../../../../styles/components'
-import * as Icon from 'phosphor-react'
+import { Coffee } from '../../../'
 
 export const Coffees = () => {
   return (
     <S.Coffees>
       <S.CoffeesList>
-        <S.CoffeeItem>
-          <div className='item__content'>
-            <img
-              className='item__img'
-              src='/coffees/expresso-tradicional.png'
-              alt='Expresso Tradicional'
-            />
-
-            <div className='item__wrapper'>
-              <strong className='item__name'>Expresso Tradicional</strong>
-
-              <div className='item__buy'>
-                <strong>R$ 9,90</strong>
-
-                <Styled.CouterButton>
-                  <input type='number' defaultValue={1} min={1} readOnly />
-                  <div>
-                    <span>--</span>
-                    <span>+</span>
-                  </div>
-                </Styled.CouterButton>
-              </div>
-            </div>
-          </div>
-
-          <S.ItemDelete>
-            <Icon.Trash />
-          </S.ItemDelete>
-        </S.CoffeeItem>
-
-        <S.CoffeeItem>
-          <div className='item__content'>
-            <img
-              className='item__img'
-              src='/coffees/expresso-tradicional.png'
-              alt='Expresso Tradicional'
-            />
-
-            <div className='item__wrapper'>
-              <strong className='item__name'>Expresso Tradicional</strong>
-
-              <div className='item__buy'>
-                <strong>R$ 9,90</strong>
-
-                <Styled.CouterButton>
-                  <input type='number' defaultValue={1} min={1} readOnly />
-                  <div>
-                    <span>--</span>
-                    <span>+</span>
-                  </div>
-                </Styled.CouterButton>
-              </div>
-            </div>
-          </div>
-
-          <S.ItemDelete>
-            <Icon.Trash />
-          </S.ItemDelete>
-        </S.CoffeeItem>
-
-        <S.CoffeeItem>
-          <div className='item__content'>
-            <img
-              className='item__img'
-              src='/coffees/expresso-tradicional.png'
-              alt='Expresso Tradicional'
-            />
-
-            <div className='item__wrapper'>
-              <strong className='item__name'>Expresso Tradicional</strong>
-
-              <div className='item__buy'>
-                <strong>R$ 9,90</strong>
-
-                <Styled.CouterButton>
-                  <input type='number' defaultValue={1} min={1} readOnly />
-                  <div>
-                    <span>--</span>
-                    <span>+</span>
-                  </div>
-                </Styled.CouterButton>
-              </div>
-            </div>
-          </div>
-
-          <S.ItemDelete>
-            <Icon.Trash />
-          </S.ItemDelete>
-        </S.CoffeeItem>
+        <Coffee />
       </S.CoffeesList>
 
       <div>
