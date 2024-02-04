7cebfecfb5f740ed3c5fce3870c63a4ae0315edb
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2024-02-03 16:00:45 -0800

    🏗feat(zod):  schema validation - new order

---
+++ src/schemas/newOrder.ts
@@ -0,0 +1,16 @@
+import * as zod from 'zod'
+
+const newOrder = zod.object({
+  cep: zod.string().min(9, 'Informe o CEP').max(9, 'Informe o CEP'),
+  street: zod.string().min(1, 'Informe a rua'),
+  number: zod.string().min(1, 'Informe o número'),
+  complement: zod.string(),
+  neighborhood: zod.string().min(1, 'Informe o bairro'),
+  city: zod.string().min(1, 'Informe a cidade'),
+  state: zod.string().min(1, 'Informe a UF'),
+  paymentMethod: zod.enum(['credit', 'debit', 'cash'], {
+    invalid_type_error: 'Informe um método de pagamento',
+  }),
+})
+
+export type NewOrderData = zod.infer<typeof newOrder>
