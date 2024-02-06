983a805d72388b501359d95cba3cebe59ffd70a9
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2024-02-06 12:29:34 -0800

    🏗feat(pages/success)🦺: url validation for the order

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,7 +1,27 @@
+import { Navigate, useParams } from 'react-router-dom'
 import { SuccessInfos } from '../../components'
 import * as S from './styles'
+import { Order } from '../../reducers/cart/reducer'
 
 function Success() {
+  const storedCartStateAsJSON = localStorage.getItem(
+    '@coffee-delivery:cart-state-1.0.0'
+  )
+
+  const { orders } = storedCartStateAsJSON
+    ? JSON.parse(storedCartStateAsJSON)
+    : { orders: [] }
+
+  const { orderId } = useParams<{ orderId: string }>()
+
+  const isValidOrderId = orders.some(
+    (order: Order) => order.id.toString() === orderId
+  )
+
+  if (!isValidOrderId) {
+    return <Navigate to='/' />
+  }
+
   return (
     <S.Success>
       <div className='sucess__container'>
